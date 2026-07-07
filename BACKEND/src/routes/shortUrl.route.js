174b1e90ff3d3e8753bcdf9a3b8af412f600196b
Ankit-cs <ankitcareer018@gmail.com>
2026-07-07 10:44:53 +0530

    updated acc to the typescript and type safety

--- BACKEND/src/routes/shortUrl.route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createShortUrl, redirectFromShortUrl } from '../controller/shortUrl.controller.js';
-const router = express.Router();
-
-router.post("/",createShortUrl);
-router.get("/:id", redirectFromShortUrl);
-
-export default router;
