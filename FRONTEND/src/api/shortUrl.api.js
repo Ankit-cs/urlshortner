4e620d8cac4140eb6198fe492454a2c0ca9618bd
Ankit-cs <ankitcareer018@gmail.com>
2026-07-07 17:24:15 +0530

    refactor: drop legacy shortUrl api

--- FRONTEND/src/api/shortUrl.api.js
+++
@@ -1,6 +0,0 @@
-import axiosInstance from "../utils/axiosinstance"
-
-export const createShortUrl = async (url,slug) =>{
-    const {data} = await axiosInstance.post("/api/create",{url,slug})
-    return data.shortUrl
-}
