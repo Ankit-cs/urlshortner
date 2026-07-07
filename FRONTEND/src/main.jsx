eaf21e3fdf86d7653c1e9cb7a6879bc8ef38eec0
Ankit-cs <ankitcareer018@gmail.com>
2026-07-07 17:23:31 +0530

    chore: update react DOM mounting

--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -1,24 +1,23 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
+import '@fontsource/inter/900.css';
 import './index.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { routeTree } from './routing/routeTree.js'
-import store from './store/store.js'
-import { Provider } from 'react-redux'
+import App from './App.jsx'
 
-export const queryClient = new QueryClient()
-const router = createRouter({
-  routeTree,
-  context:{
-    queryClient,
-    store
-  }
-})
+import { BrowserRouter } from 'react-router-dom';
+
+import { AuthProvider } from './context/AuthContext.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  </Provider>
+  <StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>,
 )
