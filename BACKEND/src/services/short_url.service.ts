1f06abd4caff25c7dffdf978e94911628fbbb4f0
Ankit-cs <ankitcareer018@gmail.com>
2026-07-07 10:55:52 +0530

    updated acc to the typescript and type safety

---
+++ BACKEND/src/services/short_url.service.ts
@@ -0,0 +1,19 @@
+import { generateNanoId } from "../utils/helper.ts"
+import urlSchema from "../models/url.model.ts"
+import { getCustomShortUrl, saveShortUrl } from "../dao/short_url_db.ts"
+
+export const createShortUrlWithoutUser = async (url: string, slug: string | null = null) => {
+    const shortUrl = generateNanoId(7)
+    if(!shortUrl) throw new Error("Short URL not generated")
+    await saveShortUrl(shortUrl,url)
+    return shortUrl
+}
+
+export const createShortUrlWithUser = async (url: string, userId: any, slug: string | null = null) => {
+    const shortUrl = slug || generateNanoId(7)
+    const exists = await getCustomShortUrl(slug || "");
+    if(exists) throw new Error("This custom url already exists")
+
+    await saveShortUrl(shortUrl,url,userId)
+    return shortUrl
+}
