bb10be7653f60c6d4912f80aefaf84035fcf9405
Ankit-cs <ankitcareer018@gmail.com>
2026-07-07 12:36:35 +0530

    updated routes

--- BACKEND/src/routes/auth.route.ts
+++ BACKEND/src/routes/auth.route.ts
@@ -1,12 +1,12 @@
-import express from "express"
-import { register_user, login_user,logout_user,get_current_user } from "../controller/auth.controller.ts"
-import { authMiddleware } from "../middleware/auth.middleware.ts"
+import { Hono } from "hono";
+import { get_current_user, login_user, logout_user, register_user } from "../controller/auth.controller.ts";
+import { authMiddleware } from "../middleware/auth.middleware.ts";
 
-const router = express.Router()
+const router = new Hono();
 
-router.post("/register", register_user)
-router.post("/login", login_user)
-router.post("/logout", logout_user)
-router.get("/me", authMiddleware,get_current_user)
+router.post("/register", register_user);
+router.post("/login", login_user);
+router.post("/logout", logout_user);
+router.get("/getuser", authMiddleware, get_current_user);
 
-export default router
+export default router;
