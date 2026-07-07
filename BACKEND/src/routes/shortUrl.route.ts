5df81d1523a8663e0eee1c09bda6541d8fd499d7
Ankit-cs <ankitcareer018@gmail.com>
2026-07-07 12:36:46 +0530

    updated shorturl route

--- BACKEND/src/routes/shortUrl.route.ts
+++ BACKEND/src/routes/shortUrl.route.ts
@@ -1,8 +1,9 @@
-import express from 'express';
-import { createShortUrl, redirectFromShortUrl } from '../controller/shortUrl.controller.ts';
-const router = express.Router();
+import { Hono } from "hono";
+import { createCustomShortUrl, createShortUrl } from "../controller/shortUrl.controller.ts";
 
-router.post("/",createShortUrl);
-router.get("/:id", redirectFromShortUrl);
+const router = new Hono();
+
+router.post("/", createShortUrl);
+router.post("/custom", createCustomShortUrl);
 
 export default router;
