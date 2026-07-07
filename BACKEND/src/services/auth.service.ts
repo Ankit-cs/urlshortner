6e2b9ccb70c505867a67546d46a1cfaddaf2184c
Ankit-cs <ankitcareer018@gmail.com>
2026-07-07 10:55:42 +0530

    updated acc to the typescript and type safety

---
+++ BACKEND/src/services/auth.service.ts
@@ -0,0 +1,23 @@
+import { createUser, findUserByEmail, findUserByEmailByPassword } from "../dao/user.dao.ts";
+import { ConflictError } from "../utils/errorHandler.ts";
+import { signToken } from "../utils/helper.ts";
+
+export const registerUser = async (name: string, email: string, password: string) => {
+    const user = await findUserByEmail(email);
+    if (user) throw new ConflictError("User already exists");
+
+    const newUser = await createUser(name, email, password);
+    const token = await signToken({ id: newUser._id });
+    return { token, user: newUser };
+};
+
+export const loginUser = async (email: string, password: string) => {
+    const user = await findUserByEmailByPassword(email);
+    if (!user) throw new Error("Invalid email or password");
+
+    const isPasswordValid = await (user as any).comparePassword(password);
+    if (!isPasswordValid) throw new Error("Invalid email or password");
+
+    const token = signToken({ id: user._id });
+    return { token, user };
+};
